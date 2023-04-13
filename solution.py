e5f232f1f3cafd3bc6e4638d1ad2df10d337a793
DaniilKalambetov123123 <84600463+DaniilKalambetov123123@users.noreply.github.com>
2023-04-13 18:06:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,5 +11,5 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return (proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[0] <= 0 and
+    return (proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[0] <= 0 or
             proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] < 0.93)
